f808677e33a0514665adcf2f0e7c558720e0310a
Chris Carr <ccarr895@gmail.com>
2018-12-17 19:11:11 +0000

    review of code for task b complete

--- Task_B/Task_B.asm
+++ Task_B/Task_B.asm
@@ -10,23 +10,14 @@ syscall
 #Store user input
 li $v0, 5
 syscall
-la $a1, ($v0)
+move $a1, $v0
 
 #divide input by 2
 div $a2, $a1, 2
 
-#Check if integer needs to be rounded up (mfhi contains any remainder), branch to relevant function
-mfhi $a3
-bgtz $a3, round
-beqz $a3, output
-
-#round integer result (we cheat and just add 1)
-round:
-add $a2, $a2, 1
-
 #output integer result
 output:
-la $a0, ($a2)
+move $a0, $a2
 li $v0, 1
 syscall
 
